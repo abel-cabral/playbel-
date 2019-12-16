5e741487bea3417332a450ee0d380f6591c76bd9
Abel Cabral <jcabral@id.uff.br>
2019-12-15 22:41:29 -0300

    Melhorias na interface e aproximação para player de audio.

--- app/src/main/java/com/abel/netflix/MainActivity.java
+++ app/src/main/java/com/abel/netflix/MainActivity.java
@@ -3,12 +3,18 @@ package com.abel.netflix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Remove title bar e menu do android
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
     }
 }
