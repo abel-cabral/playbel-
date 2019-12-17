99da5038ab04c1931cea8a0823eccb58ea48ff63
Abel Cabral <jcabral@id.uff.br>
2019-12-17 13:26:05 -0300

    Reproducao de midia e troca automatica de musica

--- app/src/main/java/com/abel/netflix/MainActivity.java
+++ app/src/main/java/com/abel/netflix/MainActivity.java
@@ -4,17 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.graphics.ColorFilter;
-import android.os.Build;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.ImageView;
 
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    boolean playing = false;
+    private MediaPlayer mediaPlayer = null;
+    private boolean playing = false;
+    private int current = 0;
+
+    private ArrayList<Integer> playlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,38 +27,105 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        playlist = new ArrayList<>();
+        playlist.add(R.raw.stand_proud);
+        playlist.add(R.raw.bloody_stream);
+
+        mediaPlayer = mediaPlayerFactory(playlist, mediaPlayer);
     }
 
-    public void playMusic(View view){
-        ImageView play = findViewById(R.id.buttonPlay);
-
-
+    public void play(View view) {
         playing = !playing;
+        // play music
+        if (mediaPlayer != null) {
+            playMusic(mediaPlayer);
+        }
 
+    }
+
+    public void next(View view) {
+        nextMusic();
+    }
+
+    public void previous(View view) {
+        previousMusic();
+    }
+
+    private void playMusic(MediaPlayer media) {
+        ImageView play = findViewById(R.id.buttonPlay);
         if (playing) {
+            media.start();
             play.setImageResource(R.drawable.pause);
         } else {
+            media.pause();
             play.setImageResource(R.drawable.ic_play_arrow_black_24dp);
         }
+        // Ao fim da musica avança para a proxima
+        media.setOnCompletionListener((res) -> nextMusic());
     }
 
-    public void nextMusic(View view){
+    private void nextMusic() {
+        if (current >= (playlist.size() - 1)) return;
         ImageView button = findViewById(R.id.nextButton);
+        current += 1;
+        mediaPlayer = mediaPlayerFactory(playlist, mediaPlayer);
+        playMusic(mediaPlayer);
         animationClick(button);
     }
 
-    public void previousMusic(View view){
+    private void previousMusic() {
+        if (current <= 0) return;
         ImageView button = findViewById(R.id.previousButton);
+        current -= 1;
+        mediaPlayer = mediaPlayerFactory(playlist, mediaPlayer);
+        playMusic(mediaPlayer);
         animationClick(button);
     }
 
-    public void animationClick(ImageView v) {
+    private void animationClick(ImageView v) {
         if (android.os.Build.VERSION.SDK_INT > 15) {
             System.out.println("Executado");
             ColorFilter currentColor = v.getColorFilter();
             v.setColorFilter(Color.YELLOW);
             v.setColorFilter(currentColor);
         }
-
     }
+
+
+
+    // UTILITARIOS
+    public MediaPlayer mediaPlayerFactory(ArrayList<Integer> playlist, MediaPlayer now) {
+        if (now != null) {
+            now.stop();
+        }
+        // Instantiating MediaPlayer class
+        MediaPlayer facMediaPlayer = MediaPlayer.create(getApplicationContext(), playlist.get(current));
+        facMediaPlayer.setVolume(80, 80);
+
+        // Subscrible para ouvir mudanças na barra de progresso
+        // simpleSeekBar.setMax(100);
+
+        /*
+        // Adding Listener to value property.
+        progressBar.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable,
+                                Number oldValue, Number newValue) {
+
+                if (progressBarSystem) {
+                    return;
+                }
+
+                mediaPlayer.pause();
+                timeScreen.setText(TimeConvert.convertToMinute(timeMusic, "mm:ss"));
+                timeMusic = mediaPlayer.getTotalDuration().toMillis() - mediaPlayer.getCurrentTime().toMillis();
+                mediaPlayer.seek(mediaPlayer.getMedia().getDuration().multiply(progressBar.getValue() / 100));
+            }
+        });
+        */
+        return facMediaPlayer;
+    }
+
+
 }
